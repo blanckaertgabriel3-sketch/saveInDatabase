ae5b040a2b5e97558d1d41815cc9c2b3099d7d61
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-02-21 18:10:51 +0100

    add form.php

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>form</title>
+</head>
+<body>
+	<form method="get" action="">
+		<input type="text" placeholder="name" name="name">
+		<button type="submit">validate</button>
+	</form>
+	<?php 
+		
+	?>
+</body>
+</html>

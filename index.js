3667efd23218bbc8ad442c0c8fbf79cd3b2e3592
gabriel <blanckaertgabriel@Gabriel>
2026-02-21 17:17:14 +0100

    add button, text box, ready to start save the click number in sql database

---
+++ index.js
@@ -0,0 +1,8 @@
+//dom
+let add = document.getElementById("add");
+let textBox = document.getElementById("textBox");
+
+// button listener
+add.addEventListener("click", () => {
+	textBox.innerHTML ++;
+})

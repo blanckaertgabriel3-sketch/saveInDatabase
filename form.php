b73ad188cfc47798752620bbedfa17d369bf3503
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-02-22 12:08:36 +0100

    input password

--- form.php
+++ form.php
@@ -6,8 +6,28 @@
 	<title>form</title>
 </head>
 <body>
-	<form method="get" action="">
+	<style>
+		body {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			min-height: 100vh;
+		}
+		#form {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			gap: 5px;
+		}
+		button {
+			width: 100px;
+			height: 20px;
+		}
+	</style>
+	<form id="form" method="get" action="">
 		<input type="text" placeholder="name" name="name">
+		<input type="password" placeholder="password" name="password">
 		<button type="submit">validate</button>
 	</form>
 	<?php 
